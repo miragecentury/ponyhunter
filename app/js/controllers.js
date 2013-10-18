ff775677cf6661374a15b0284c90ab2d0b67de0d
miragecentury <victorien.vanroye@gmail.com>
2013-10-18 20:32:25 +0200

    - pub

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,47 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-    
-  }])
-  .controller('MyCtrl2', [function() {
+        controller('MyCtrl1', ["$scope",function(scope) {
+        var MAX_CLOUD = 6;
 
-  }]);
+        scope.watt = 0;
+        scope.cloud_nb = 0;
+
+        var animate_sun = function(){
+            
+        };
+
+        var animate_cloud = function() {
+
+        };
+
+        scope.garbage = {
+            solar_panel: {
+                nb: 0,
+                initPrize: 100,
+                nbBuy: 0,
+                getPrize: function() {
+                    return scope.garbage.solar_panel.nb * scope.garbage.solar_panel.initPrize;
+                }
+            },
+            sainte_grenade: {
+                nb: 0,
+                initPrize: 1000,
+                nbBuy: 0,
+                getPrize: function() {
+                    return scope.garbage.sainte_grenade.nb * scope.garbage.sainte_grenade.initPrize;
+                }
+            },
+            pegaze: {
+                nb: 0,
+                initPrize: 500,
+                nbBuy: 0,
+                getPrize: function() {
+                    return scope.garbage.pegaze.nb * scope.garbage.pegaze.initPrize;
+                }
+            }
+        };
+    }])
+        .controller('MyCtrl2', [function() {
+
+    }]);
